ed5788cb25a969886b206fcc48b2462bbbed05d6
Vladislav <wlladisla@gmail.com>
2023-02-02 23:42:36 +0300

    init: start project, add configs

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//import { Routes, Route } from 'react-router-dom';
+
+export const App = () => {
+  return (
+    <h1>Проект взлетел ! </h1>
+
+    // <Routes>
+    //   <Route
+    //     path="/"
+    //     element={}/>
+    //   <Route/>
+    //   </Routes>
+  );
+};
