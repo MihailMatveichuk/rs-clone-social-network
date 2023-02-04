cd59118d1a6eb174b165ba102a9d6defd7a79468
Mikhail <mihkailmatveichuk@gmail.com>
2023-02-04 23:47:40 +0300

    add: message file

---
+++ src/components/Message.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+const Rogan = require('./assets/images/Rogan.png');
+
+const Message = () => {
+  return (
+    <div className="message">
+      <div className="message-info">
+        <img src={Rogan} alt="" />
+      </div>
+      <div className="message-content">
+        <span style={{ fontSize: '14px', fontWeight: 700 }}>
+          Joe Rogan{' '}
+          <span style={{ fontSize: '10px', fontWeight: 300 }}>9:59 PM</span>
+        </span>
+        <span className="message-text">
+          Thanks for doing this, Elon Musk. Really appreciate it.
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
