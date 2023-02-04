9d2610f51a9ed5f2d06c25ce14309071c3d54a84
Mikhail <mihkailmatveichuk@gmail.com>
2023-02-04 23:50:40 +0300

    add: sidebar file

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+import '../App.css';
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <Navbar />
+    </div>
+  );
+};
+
+export default SideBar;
