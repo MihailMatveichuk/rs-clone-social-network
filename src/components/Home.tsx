e06f12d6eea37271a73b7e1ada6c2979c70f39be
Gennady Kashin <gennady.kashin94@gmail.com>
2023-02-09 14:04:52 +0300

    feat: add sign in vie email and phone

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,8 +8,8 @@ const Home = () => {
   return (
     <div className="home">
       <div className="container">
-        <SideBar />
-        <Chat />
+        {/* <SideBar /> */}
+       {/* <Chat /> */}
       </div>
     </div>
   );
--- src/firebase.ts
+++ src/firebase.ts
@@ -8,15 +8,23 @@ import { getStorage } from 'firebase/storage';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyADJ4V9JyV9dgcaXLBe45okZ9qKNyisnPs',
+//   authDomain: 'rs-chat-9d2b3.firebaseapp.com',
+//   projectId: 'rs-chat-9d2b3',
+//   storageBucket: 'rs-chat-9d2b3.appspot.com',
+//   messagingSenderId: '794508097033',
+//   appId: '1:794508097033:web:534101952cde4972f0aad8',
+// };
 const firebaseConfig = {
-  apiKey: 'AIzaSyADJ4V9JyV9dgcaXLBe45okZ9qKNyisnPs',
-  authDomain: 'rs-chat-9d2b3.firebaseapp.com',
-  projectId: 'rs-chat-9d2b3',
-  storageBucket: 'rs-chat-9d2b3.appspot.com',
-  messagingSenderId: '794508097033',
-  appId: '1:794508097033:web:534101952cde4972f0aad8',
+  apiKey: "AIzaSyDuBOLqhKjpp55463UHvDPc7KnA46dza3Y",
+  authDomain: "rs-chat-da1ad.firebaseapp.com",
+  databaseURL: "https://rs-chat-da1ad-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "rs-chat-da1ad",
+  storageBucket: "rs-chat-da1ad.appspot.com",
+  messagingSenderId: "712962934357",
+  appId: "1:712962934357:web:3032efae27e345b32e76dc"
 };
-
 // Initialize Firebase
 
 export const app = initializeApp(firebaseConfig);
